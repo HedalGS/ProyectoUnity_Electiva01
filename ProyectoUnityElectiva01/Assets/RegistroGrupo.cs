6d32cb278cf5272ccdbd6b296ff905ff2ffd0108
Margarita Martinez Camacho <est.margarita.mart@unimilitar.edu.co>
2025-07-26 16:02:56 -0500

    agregando segundo participante del grupo
    poniendo el nombre del segundo participante del grupo

--- ProyectoUnityElectiva01/Assets/RegistroGrupo.cs
+++ ProyectoUnityElectiva01/Assets/RegistroGrupo.cs
@@ -5,4 +5,5 @@ public class RegistroGrupo : MonoBehaviour
     //	Nombre completo: [Daniel Hernando Gil Cusi]	
     //	Fecha: [23/07/2025]	
     //	Hora: [21:16]	
+    //  Nombre completo: [Margarita Maria Martinez Camacho]
 }
